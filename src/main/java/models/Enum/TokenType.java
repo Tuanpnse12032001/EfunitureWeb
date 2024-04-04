db2f3ed5170caab30fac0df4c0ce2da6cf1778bd
Tuanpnse12032001 <tuanpn12301@gmail.com>
2024-04-04 11:21:43 +0700

    first Project Setting

---
+++ src/main/java/models/Enum/TokenType.java
@@ -0,0 +1,5 @@
+package models.Enum;
+
+public enum TokenType {
+    BEARER,
+}
--- src/main/java/org/example/efunitureweb/EfunitureWebApplication.java
+++ src/main/java/org/example/efunitureweb/EfunitureWebApplication.java
@@ -1,13 +1,33 @@
 package org.example.efunitureweb;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class EfunitureWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EfunitureWebApplication.class, args);
+	}
+
+	@Bean
+	public Cloudinary cloudinaryConfig() {
+		Cloudinary cloudinary = null;
+		Map config = new HashMap<>();
+		config.put("cloud_name", "dwqq0mx4j");
+		config.put("api_key", "485134685654857");
+		config.put("api_secret", "lFJj6FmcE2owKBgm_1UGvb4-m6M");
+
+		cloudinary = new Cloudinary(config);
+		return cloudinary;
+
+
 	}
 
 }
