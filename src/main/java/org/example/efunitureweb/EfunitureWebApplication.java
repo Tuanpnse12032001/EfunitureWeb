eb3cb704d51e60baa57ec58a749f6b48af56ec4a
Tuanpnse12032001 <tuanpn12301@gmail.com>
2024-04-04 11:15:37 +0700

    first push

---
+++ src/main/java/org/example/efunitureweb/EfunitureWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.efunitureweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfunitureWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfunitureWebApplication.class, args);
+	}
+
+}
